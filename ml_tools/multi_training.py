803d98009c80b6ac1985d2ef2e15aebc9e1d955d
Niccolo Picchiarelli <picchiarelliniccolo@gmail.com>
2026-03-11 11:41:07 +0100

    code refactory: move train_opt to ml_tools.multi_training

---
+++ ml_tools/multi_training.py
@@ -0,0 +1,46 @@
+import copy
+import numpy as np
+import torch
+from tqdm import tqdm
+
+def reset_weights(m, seed):
+    if hasattr(m, 'reset_parameters'):
+        torch.manual_seed(seed)
+        torch.nn.init.xavier_normal_(m.weight)
+        torch.nn.init.zeros_(m.bias)
+
+def train_opt(model_fun, opt_configs, criterion, loader, training_repetitions, epochs, device):
+    results = {}
+
+    for i, opt_name in enumerate(opt_configs):
+        model = model_fun().to(device)
+        opt_fun = opt_configs[opt_name]
+        optimizer = opt_fun(model.parameters())
+        print(f"Training with optimizer {opt_name}...")
+        loss_history = np.zeros((epochs,training_repetitions))
+        best_loss = float('inf')
+        
+        for rep in range(training_repetitions):
+            model.apply(lambda m: reset_weights(m, seed=rep+610))  # reset weights with different seed for each repetition
+
+            pbar = tqdm(range(epochs), desc="Training",)
+            for epoch in pbar:
+                epoch_loss = 0.0
+                for x_batch, y_batch in loader:
+                    optimizer.zero_grad()
+                    y_pred = model(x_batch)
+                    loss = criterion(y_pred, y_batch)
+                    loss.backward()
+                    optimizer.step()
+                    epoch_loss += loss.item() * len(x_batch)
+                    if epoch_loss < best_loss:
+                        best_loss = epoch_loss
+                        best_model_state = copy.deepcopy(model.state_dict())
+
+                epoch_loss /= len(loader.dataset)  # average loss over dataset
+                loss_history[epoch, rep] = epoch_loss  # save loss for this epoch
+                if (epoch) % 10 == 0:
+                    pbar.set_postfix(mse=f"{epoch_loss:.6f}")
+        model.load_state_dict(best_model_state)
+        results[opt_name] = {"model": model, "opt_state": optimizer.state_dict(), "loss_history_avg": np.mean(loss_history, axis=1), "loss_history_std": np.std(loss_history, axis=1)}
+    return results
